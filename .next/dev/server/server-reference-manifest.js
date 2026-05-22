4d5282cbc46e4b9fe5245d9d58454b76ab171096
Anshika Shrivastava <anshikashrivastava7654@gmail.com>
2026-05-22 15:51:20 +0530

    remove node_modules from tracking

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"F0bz1AbBFq5MCp4hnTi9/VILBeVqx67HvzCjJaxtocU=\"\n}"
--- .next/dev/static/chunks/src_app_results_page_tsx_0-0lnb2._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_0g1i5br._.js","static/chunks/src_0znxmnt._.js"],
-    source: "dynamic"
-});
